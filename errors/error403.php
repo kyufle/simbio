96ee6510cdafedb06b95432cb503af01bb924e09
valeriasm2 <vsantanamegia.cf@iesesteveterradas.cat>
2026-01-16 20:36:13 +0100

    errores idiomas navegador

---
+++ errors/error403.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>403 nope</title>
+    <link rel="stylesheet" href="/styles.css?v=<?php echo time(); ?>">
+</head>
+<body class="page-error">
+    <div class="contenedor">
+        <h1 class="titul">
+            ERROR 403
+        </h1>
+        <p class="text"></p>
+    </div>
+</body>
+</html>
+
---
+++ errors/error404.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>404 nope</title>
+    <link rel="stylesheet" href="/styles.css?v=<?php echo time(); ?>">
+</head>
+<body class="page-error">
+    <div class="contenedor">
+        <h1>ERROR 404</h1>
+        <p>Uep! Aquesta pàgina no existeix...</p>
+    </div>
+</body>
+</html>
+
