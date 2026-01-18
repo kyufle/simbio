f7e5fa8f14395878c081cd5162c51a9cb4537863
valeriasm2 <vsantanamegia.cf@iesesteveterradas.cat>
2026-01-18 20:32:48 +0100

    spec18 completada

--- errors/error404.php
+++ errors/error404.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="ca">
 <head>
-    <title>404 nope</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pàgina no trobada - 404</title>
     <link rel="stylesheet" href="/styles.css?v=<?php echo time(); ?>">
 </head>
 <body class="page-error">
     <div class="contenedor">
         <h1>ERROR 404</h1>
         <p>Uep! Aquesta pàgina no existeix...</p>
+        <a href="/login.php" class="button">Torna a l'inici</a>
     </div>
 </body>
 </html>
