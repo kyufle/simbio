a1f43175e4a7cf38aab2566ef544ca7d56314f35
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-01-18 22:27:11 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
 <?php
+require_once 'includes/auth.php';
 require_once 'includes/bd_profile.php';
 require_once 'includes/logger.php';
-require_once 'includes/auth.php';
 
 // Si no està connectat, redirigeix a login.php
 if (!isLogged()) {
