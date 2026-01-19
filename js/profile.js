4904e6d7efa7bdad1f7eb7e419e34946b76a2122
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-01-19 16:00:39 +0100

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -30,10 +30,10 @@ async function displayUserProjects(userEmail) {
         projectElement.classList.add('project');
 
         const projectLink = document.createElement('a');
-        projectLink.href = `preview_video.html?video_path=${encodeURIComponent(project.video_path)}`;
+        projectLink.href = `preview_video.html?video=${encodeURIComponent(project.video_path)}`;
 
         const projectImage = document.createElement('img');
-        projectImage.src = project.image_path;
+        projectImage.src = project.image;
         projectImage.alt = project.title;
         projectImage.classList.add('project-preview');
 
