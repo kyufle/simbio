ee29cd17eb517574ae3867ca9d9615accd81b69a
Dpcrack22 <davidperera2006@gmail.com>
2026-01-20 21:37:02 +0100

    Create test_mail.php

---
+++ test_mail.php
@@ -0,0 +1,11 @@
+<?php
+$to = 'TU_CORREO@EJEMPLO.COM'; // Cambia esto por tu correo real
+$subject = 'Prueba de mail() desde PHP';
+$message = "Esto es un test de la función mail() en tu servidor.\nSi recibes este correo, mail() funciona correctamente.";
+$headers = "From: test@simbio.local\r\n";
+
+if (mail($to, $subject, $message, $headers)) {
+    echo "Correo enviado correctamente a $to";
+} else {
+    echo "Fallo al enviar el correo";
+}
