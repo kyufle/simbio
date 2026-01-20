c210d736bdaa31c771ed92a3b312434c7ece3211
Dpcrack22 <davidperera2006@gmail.com>
2026-01-20 21:38:21 +0100

    Update test_mail.php

--- test_mail.php
+++ test_mail.php
@@ -1,5 +1,5 @@
 <?php
-$to = 'TU_CORREO@EJEMPLO.COM'; // Cambia esto por tu correo real
+$to = 'davidperera2006@gmail.com'; // Cambia esto por tu correo real
 $subject = 'Prueba de mail() desde PHP';
 $message = "Esto es un test de la función mail() en tu servidor.\nSi recibes este correo, mail() funciona correctamente.";
 $headers = "From: test@simbio.local\r\n";
