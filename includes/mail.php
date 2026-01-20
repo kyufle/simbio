f117c93573a07c9b112b79ac7a26003275f9392d
Dpcrack22 <davidperera2006@gmail.com>
2026-01-20 21:06:37 +0100

    Update mail.php

--- includes/mail.php
+++ includes/mail.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/logger.php';
 /**
  * Envia el correu de validació de registre
  */
+function sendRegistrationEmail(string $userEmail, string $userName, string $validationToken): bool
 {
     $validateLink = "http://localhost/register.php?validate=" . urlencode($validationToken);
 
@@ -24,14 +25,14 @@ require_once __DIR__ . '/logger.php';
     $message .= "Hola {$userName},\n\n";
     $message .= "Per activar el teu compte visita:\n";
     $message .= $validateLink . "\n\n";
-     $message .= "Aquest enllaç caduca en 30 minuts.\n\n";
+    $message .= "Aquest enllaç caduca en 30 minuts.\n\n";
     $message .= "--{$boundary}\r\n";
     $message .= "Content-Type: text/html; charset=UTF-8\r\n\r\n";
     $message .= "
         <p>Hola <strong>{$userName}</strong>,</p>
         <p>Per activar el teu compte fes clic aquí:</p>
         <p><a href='{$validateLink}'>Activar compte</a></p>
-          <p><small>L'enllaç caduca en 30 minuts.</small></p>
+        <p><small>L'enllaç caduca en 30 minuts.</small></p>
     ";
     $message .= "\r\n--{$boundary}--";
 
