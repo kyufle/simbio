55c720df54637dd5e2405f686f1c43a23f555857
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-01-27 21:44:19 +0100

    Prueba de logout

--- admin/logout.php
+++ admin/logout.php
@@ -3,5 +3,5 @@ require_once '/../includes/auth.php';
 require_once '/../includes/logger.php';
 
 logoutAdmin();
-header('Location: login.php');
-exit;
+header('Location: login.php', true, 302);
+exit();
