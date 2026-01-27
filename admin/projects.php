40f0c98e4a5d42b1f55605a2bd6a419318db11cc
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-01-27 20:02:26 +0100

    Update projects.php

--- admin/projects.php
+++ admin/projects.php
@@ -133,7 +133,6 @@ if (!isAdminLoggedIn()) {
         }
     </style>
 </head>
-<body class="login-page-admin">
 <header>
     <nav>
         <a href="index.php" class="nav-link">Inici</a>
@@ -143,6 +142,7 @@ if (!isAdminLoggedIn()) {
         <a href="login.php" class="nav-link">Tancar sessió</a>
     </nav>
 </header>
+<body class="login-page-admin">
 <main>
     <h1>Gestió de Projectes</h1>
     <div id="user-projects"></div>
