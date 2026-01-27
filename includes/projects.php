1a4c1b920905501baab722c7392c9d53856aa02b
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-01-27 16:34:46 +0100

    Update projects.php

--- includes/projects.php
+++ includes/projects.php
@@ -80,8 +80,6 @@ try {
             'tags' => $projectTags,
             'match' => $tagsEnComun > 0, // TRUE solo si comparten tags
             'tags_en_comun' => $tagsEnComun,
-            'liked' => $isLiked
-            'match' => ($project['possible_match'] == 1), // 💖 esto activa el icono
             'liked' => $isLiked,
             'user_id' => $project['user_id'] // Añadido para enlaces de chat
         ];
