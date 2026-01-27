9371944028a7617bb3180382f66309a61306a39e
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-01-27 19:39:07 +0100

    Update projects_action.php

--- admin/projects_action.php
+++ admin/projects_action.php
@@ -3,10 +3,10 @@
     if (isset($_GET['action'], $_GET['id'])) {
         $project_id = (int)$_GET['id'];
         if ($_GET['action'] === 'delete') {
-            $stmt = $db->prepare("UPDATE projects SET deleted=1 WHERE project_id=?");
+            $stmt = $db->prepare("UPDATE project SET deleted=1 WHERE project_id=?");
             $stmt->execute([$project_id]);
         } elseif ($_GET['action'] === 'restore') {
-            $stmt = $db->prepare("UPDATE projects SET deleted=0 WHERE project_id=?");
+            $stmt = $db->prepare("UPDATE project SET deleted=0 WHERE project_id=?");
             $stmt->execute([$project_id]);
         }
     }
